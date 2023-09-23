2052bda3b231b09a0e1cc06e0c393533dd2db4f3
Márcio Martins <marciocmartins@gmail.com>
2023-09-23 10:37:27 -0300

    Aggregation Binding
    Vamos utilizar um arquivo no formato JSON para simular um backend e utilizar o controle sap.m.List na view para consumir este novo modelo e mostrar os dados em uma listagem.

--- aggregation-binding/controller/App.controller.js
+++ aggregation-binding/controller/App.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
  ], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("udemy.openui5.passoapasso.controller.App", {
-      onInit : function () {
+       onInit : function () {
           // set data model on view
           var oData = {
              recipient : {
@@ -15,17 +15,31 @@ sap.ui.define([
           };
           var oModel = new JSONModel(oData);
           this.getView().setModel(oModel);
+          
+          var oInvoice = new JSONModel("Invoices.json");
+          this.getView().setModel(oInvoice, "invoice");
+          
           // set i18n model on view
           var i18nModel = new ResourceModel({
              bundleName: "udemy.openui5.passoapasso.i18n.i18n"
           });
           this.getView().setModel(i18nModel, "i18n");
        },
+    	 
        onShowHello : function () {
-          // read msg from i18n model
+       	
+       	/*
+       	internacionalização em português
+       	internationalization em inglês
+       	i123456789012345678n
+       	i18n
+       	*/
+       	
+       	// read msg from i18n model
           var oBundle = this.getView().getModel("i18n").getResourceBundle();
           var sRecipient = this.getView().getModel().getProperty("/recipient/name");
           var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
           // show message
           MessageToast.show(sMsg);
        }
